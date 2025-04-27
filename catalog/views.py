4ca35ba17fa39affbb57502daa3aa79664119536
Michael <sorokajr678@bk.ru>
2025-04-27 17:03:46 +0300

    add post method

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
 def home(request):
     return render(request, 'catalog/home.html')
 
 
 def contacts(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
+        # Здесь мы просто возвращаем простой ответ
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'catalog/contacts.html')
