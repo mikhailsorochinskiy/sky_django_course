e5d4c65a789a99ba24ef317c103b7622931e5162
Michael <sorokajr678@bk.ru>
2025-05-08 21:20:03 +0300

    add all tasks

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_detail
+
+app_name = 'catalog'
 
 urlpatterns = [
     path('home/', home),
     path('contacts/', contacts),
+    path('product_detail/<int:product_id>', product_detail, name='product_detail'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
+    path('catalog/', include('catalog.urls', namespace='catalog'),),
 ]
 
 if settings.DEBUG:
