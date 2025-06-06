75499e50e8130e8992301a17fb683c847b79d7e6
Michael <sorokajr678@bk.ru>
2025-06-06 20:26:32 +0300

    add task for owners

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -22,6 +23,7 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
     is_active = models.BooleanField(verbose_name='Статус публикации', default=False)
+    owner = models.ForeignKey(User, verbose_name='Владелец', blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name}'
